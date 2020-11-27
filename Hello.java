429cefaf346ccf72982ab29379237b0a20e98cca
Poojakashi <74890395+Poojakashi@users.noreply.github.com>
2020-11-27 12:56:00 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package firstdemo;
+
+
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("My name is Pooja");
+		// TODO Auto-generated method stub
+
+	}
+
+}
